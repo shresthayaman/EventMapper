41367b68f1661862dddb407f8984ce3dff8aecc7
shresthayaman <shresthayaman@hotmail.com>
2018-05-30 11:32:33 -0400

    Created a proper Map component that renders a map

--- eventmapper/src/Components/RenderMap.js
+++ eventmapper/src/Components/RenderMap.js
@@ -4,20 +4,24 @@ import { Marker } from "react-mapbox-gl";
 import { Popup } from "react-mapbox-gl";
 import { Cluster } from "react-mapbox-gl";
 
-const map = ReactMapboxGl({
+const Map = ReactMapboxGl({
   accessToken:
     "pk.eyJ1IjoiaWFubWNjcmF5IiwiYSI6ImNqaHMyd291NTIxMHYzN3BsdzVsMmQ5Z3gifQ.H8dAYkBnMpyT5942k_1qfQ"
 });
 
-export default class Map extends React.Component {
+class RenderMap extends React.Component {
+  contructor(props) {
+    this.state = {};
+  }
   render() {
     return (
       <Map
         style="mapbox://styles/ianmccray/cjhs3nc19077q2sk81qpjrvwk"
-        containerStyle={{ width: "100vw", height: "100vh" }}
+        containerStyle={{ width: "75vw", height: "100vh", left: "500px" }}
         center={[-78.5, 38.03]}
         zoom={[15]}
       />
     );
   }
 }
+export default RenderMap;
