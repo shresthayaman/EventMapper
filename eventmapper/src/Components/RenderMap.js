11c7084e8c76210015cc1fe3c3eeff578efc2744
shresthayaman <shresthayaman@hotmail.com>
2018-06-01 11:48:34 -0400

    added popups

--- eventmapper/src/Components/RenderMap.js
+++ eventmapper/src/Components/RenderMap.js
@@ -16,7 +16,9 @@ class RenderMap extends React.Component {
       longitude: this.props.apiFeedData.currentEvent.longitude,
       latitude: this.props.apiFeedData.currentEvent.latitude,
       zoom: this.props.apiFeedData.currentEvent.zoom,
-      title: this.props.apiFeedData.currentEvent.title
+      title: this.props.apiFeedData.currentEvent.title,
+      popupcoord: [135.0, 82.8628],
+      popupevent: {}
     };
   }
 
@@ -43,6 +45,32 @@ class RenderMap extends React.Component {
         center={[this.state.longitude, this.state.latitude]}
         zoom={[this.state.zoom]}
       >
+        <Popup
+          coordinates={this.state.popupcoord}
+          offset={{
+            "bottom-left": [12, -38],
+            bottom: [0, -38],
+            "bottom-right": [-12, -38]
+          }}
+        >
+          <p1>
+            <u>
+              <b>{this.state.popupevent.title}</b>
+            </u>
+            <br />
+            <b>Location: </b>
+            {this.state.popupevent.venue_address},{" "}
+            {this.state.popupevent.city_name},{" "}
+            {this.state.popupevent.region_abbr}{" "}
+            {this.state.popupevent.postal_code}
+            <br />
+            <b>Venue: </b>
+            {this.state.popupevent.venue_name}
+            <br />
+            <b> When: </b>
+            {this.state.popupevent.start_time}
+          </p1>
+        </Popup>
         {this.props.apiFeedData.currentApiData.map(event => {
           return (
             <div>
@@ -50,11 +78,10 @@ class RenderMap extends React.Component {
                 coordinates={[event.longitude, event.latitude]}
                 anchor="bottom"
                 onClick={e => {
-                  this.setState({ title: event.title });
-                  <Popup coordinates={[event.longitude, event.latitude]}>
-                    {" "}
-                    <p1> {this.state.title} </p1>{" "}
-                  </Popup>;
+                  this.setState({
+                    popupcoord: [event.longitude, event.latitude],
+                    popupevent: event
+                  });
                 }}
               >
                 <img src={"http://maps.google.com/mapfiles/ms/icons/red.png"} />
